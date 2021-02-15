a3796d90cb7c01992a2ffaea4661de19952bb803
iamleonardjeong <iamleonardjeong@gmail.com>
2021-02-15 23:30:19 +0900

    update frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* path에 배열로 넣어주면 한 라우트 컴포넌트에 여러 개 경로를 설정할 수 있다. */}
+      {/* /@:username -> http://localhost:3000/@hoon 같은 경로에서 hoon을 username 파라미터로 읽을 수 있게 해준다. */}
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
   );
 }
 
---
+++ src/components/common/auth/AuthTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// 회원가입, 로그인 페이지의 레이아웃 담당 컴포넌트
+
+const AuthTemplateBlock = styled.div``;
+
+const AuthTemplate = () => {
+  return <AuthTemplateBlock></AuthTemplateBlock>;
+};
+
+export default AuthTemplate;
